0b233b8cdcbae39df4d97fff50fe90f8229e88b7
Milan Slancik <milan.slancik@floorpad.com>
2017-07-04 16:34:38 +0100

    Badass component working fine

--- src/components/hsbc-ui/SpecialNotification.js
+++ src/components/hsbc-ui/SpecialNotification.js
@@ -2,16 +2,18 @@ import React from 'react';
 import './SpecialNotification.css';
 import HSBC_logo from './HSBC_logo.png';
 
-const SpecialNotification = ({name, actionClick}) => {
+const SpecialNotification = (props) => {
 
-  // const onClick = (event) => {
-  //   alert(`Have a nice day ${name}`);
-  // }
+  const { name, actionClick } = props;
+
+  const onClick = (event) => {
+    alert(`Have a nice day ${name}`);
+  }
   return (
     <div className="layout">
       <img src={HSBC_logo} className="hsbc-logo" alt="logo" />
-      <i>I am a badass HSBC react component</i>
-      <button onClick={actionClick} className="button"> Click me now! </button>
+      <i>I am a badass HSBC component!</i>
+      <button onClick={typeof(actionClick) !== 'undefined' ? actionClick : onClick} className="button"> Click me now! </button>
     </div>
   );
 };
