ead3a81dc6b40e36315cc136fc84e59a1da1df26
Milan Slancik <milan.slancik@floorpad.com>
2017-07-04 17:10:09 +0100

    Links to components

--- stories/index.js
+++ stories/index.js
@@ -8,7 +8,7 @@ import { Button, Welcome } from '@storybook/react/demo';
 
 import BadassComponent from '../src/components/hsbc-ui/BadassComponent';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('BadassComponent')} />);
 
 storiesOf('HSBC UI components', module)
   .add('Badass component', () => <BadassComponent name="anybody" actionClick={action('clicked')} />);
