189076f0a1dec268f394b8e2241a085bc00fc1cc
Milan Slancik <milan.slancik@floorpad.com>
2017-07-13 11:59:33 +0100

    Added unit tests + clean up

--- src/components/form/InputContainer/index.js
+++ src/components/form/InputContainer/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './index.css';
-import InputField from '../Input';
+import InputField from '../InputField';
 
 class InputContainer extends Component {
 
