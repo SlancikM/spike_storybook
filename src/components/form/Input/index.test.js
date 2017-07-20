61e854c1b4af397fd3013891583908c5caf371ca
Milan Slancik <milan.slancik@floorpad.com>
2017-07-20 16:12:58 +0100

    Added some tests

--- src/components/form/Input/index.test.js
+++ src/components/form/Input/index.test.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom';
 import InputField from './';
 import { shallow } from 'enzyme';
 
-it('<InputField /> renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<InputField />, div);
-});
+describe('<InputField /> ', () => {
+  it('should render without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<InputField />, div);
+  });
 
-it('should render one input with the correct class applied', () => {
-  const wrapper = shallow(<InputField />);
-  expect(wrapper.find('input').hasClass('input')).toEqual(true);
+  it('should render one input with the correct class applied', () => {
+    const wrapper = shallow(<InputField />);
+    expect(wrapper.find('input').hasClass('input')).toEqual(true);
+  });
 });
--- src/components/form/InputField/index.test.js
+++ src/components/form/InputField/index.test.js
@@ -5,14 +5,16 @@ import { shallow, mount } from 'enzyme';
 
 describe('<InputField /> ', () => {
 
-  it(' should render without crashing', () => {
+  it('should render without crashing', () => {
     const div = document.createElement('div');
     ReactDOM.render(<InputField />, div);
   });
 
-  it('should have 1 children - input field', () => {
-    const wrapper = shallow(<InputField />);
-    expect(wrapper.children()).toHaveLength(1);
+  it('should have 2 children - input field & inline validation', () => {
+    const wrapper = mount(<InputField />);
+    wrapper.setState({ isError: true, });
+    wrapper.update();
+    expect(wrapper.children()).toHaveLength(2);
   });
 
 });
