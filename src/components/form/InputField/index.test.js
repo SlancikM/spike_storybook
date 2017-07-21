66b80447226eab8689178a9293bda08cb3570ad0
Milan Slancik <milan.slancik@floorpad.com>
2017-07-21 14:26:17 +0100

    Finished unit tests , in total 25

--- src/components/form/InputField/index.test.js
+++ src/components/form/InputField/index.test.js
@@ -17,4 +17,27 @@ describe('<InputField /> ', () => {
     expect(wrapper.children()).toHaveLength(2);
   });
 
+  it('should have inline error notification', () => {
+    const wrapper = mount(<InputField />);
+    wrapper.setState({ isError: true, });
+    wrapper.update();
+    expect(wrapper.find(".typeIcon__error")).toHaveLength(1);
+  });
+
+  it('should have inline info notification', () => {
+    const wrapper = mount(<InputField />);
+    wrapper.setState({ isInfo: true, });
+    wrapper.update();
+    expect(wrapper.find(".typeIcon__info")).toHaveLength(1);
+  });
+
+  it('should NOT have inline warning notification', () => {
+    const wrapper = mount(<InputField />);
+    wrapper.setState({ isWarning: true, });
+    wrapper.update();
+    expect(wrapper.find(".typeIcon__warning")).toHaveLength(0);
+  });
+
+
+
 });
